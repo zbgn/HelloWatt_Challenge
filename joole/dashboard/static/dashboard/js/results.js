19f33064c0bf4a152057f1d9cff645d196b49cdf
Gysco <zackary.beaugelin@epitech.eu>
2018-08-13 14:28:05 +0200

    Update challenge completed
    This reverts commit b58a84ae7bb70f265405f5d80907f8446bcd078e.

--- joole/dashboard/static/dashboard/js/results.js
+++ joole/dashboard/static/dashboard/js/results.js
@@ -1,1 +1,13 @@
-// ----> YOUR CODE HERE <----
+var labels = ["2017-01", "2017-02", "2017-03", "2017-04", "2017-05", "2017-06", "2017-07", "2017-08", "2017-09", "2017-10", "2017-11", "2017-12"]
+var dataChart = []
+
+for (var i = 0; i < labels.length; i++) {
+  dataChart.push({year: labels[i], data: conso_watt[11+i]});
+}
+Morris.Line({
+  element: 'chart-watt-conso',
+  data: dataChart,
+  xkey: 'year',
+  ykeys: ['data'],
+  labels: ['Watts']
+});
--- joole/dashboard/views.py
+++ joole/dashboard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
 from .forms import ClientForm
@@ -16,6 +17,7 @@ class ClientFormView(View):
             client_id = form.cleaned_data['client']
             return redirect('dashboard:results', client_id=client_id)
 
+
 def results(request, client_id):
     conso_euro = []
     conso_watt = []
@@ -23,10 +25,24 @@ def results(request, client_id):
     is_elec_heating = True
     dysfunction_detected = False
 
-    ###################################
-    # ----> YOUR CODE GOES HERE <---- #
-    ###################################
-
+    for euro in list(Conso_eur.objects.filter(client_id=client_id).values()):
+        x = [euro['janvier'], euro['fevrier'], euro['mars'], euro['avril'], euro['mai'], euro['juin'],
+             euro['juillet'], euro['aout'], euro['septembre'], euro['octobre'], euro['novembre'], euro['decembre']]
+        conso_euro.extend(x)
+    for watt in list(Conso_watt.objects.filter(client_id=client_id).values()):
+        x = [watt['janvier'], watt['fevrier'], watt['mars'], watt['avril'], watt['mai'], watt['juin'],
+             watt['juillet'], watt['aout'], watt['septembre'], watt['octobre'], watt['novembre'], watt['decembre']]
+        conso_watt.extend(x)
+    annual_costs = [sum(conso_euro[:12]), sum(conso_euro[12:])]
+    winter = (conso_watt[0] + conso_watt[1] + conso_watt[11] +
+              conso_watt[12] + conso_watt[13] + conso_watt[-1]) / 6
+    summer = (sum(conso_watt[2:11]) + sum(conso_watt[14:23])) / 18
+    is_elec_heating = (winter / summer > 1.5)
+    dysfunction_detected = (
+        annual_costs[0] / annual_costs[1] > 1.5) or (annual_costs[1] / annual_costs[0] > 1.5)
+    if dysfunction_detected:
+        messages.add_message(
+            request, messages.WARNING, 'Un dysfonctionnement sur votre installation éléctrique a été détecté.')
     context = {
         "conso_euro": conso_euro,
         "conso_watt": conso_watt,
